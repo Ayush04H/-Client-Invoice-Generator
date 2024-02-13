fcb7610d2d184e5cc5815b0c2c35d1892ef70705
Ayush Kumar Srivastava <ayush050419.com>
2024-02-13 16:40:44 +0530

    16.40 commit

--- automate.py
+++ automate.py
@@ -1,4 +1,5 @@
 from docxtpl import DocxTemplate
+
 doc=DocxTemplate("Sample Invoice_1.docx")
 doc.render({"name_company":"FC BARCELONA","address":"Bengaluru,India","pan":"UTR88965231513212","gst_no":"45698712",
             "end_date":"31th September, 2023","beginning_date":"20th September, 2023",
